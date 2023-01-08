dba99741036cbc45d65c40705aaeef56290681bc
runrun0704 <satoru0704.f@gmail.com>
2023-01-08 11:40:40 +0900

    create model Good Comment

--- backend/tweet/models.py
+++ backend/tweet/models.py
@@ -7,20 +7,20 @@ class Tweet(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=30)
     body = models.TextField()
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE, related_name='users')
-    good_count = models.IntegerField(null=True)
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE, related_name='tweet_users')
+    good_count = models.IntegerField(null=True, default=0)
     tags = models.ManyToManyField(Tag, through="TweetTag", null=True)
 
 SEX = (
-    (1, 'Mail'),
-    (2, 'Femail')
+    ('M', 'Mail'),
+    ('F', 'Femail')
 )
 class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=30)
     domain = models.CharField(max_length=100)
     password = models.CharField(max_length=20)
-    sex = models.IntegerField(choices=SEX)
+    sex = models.CharField(max_length=1, choices=SEX)
     age = models.IntegerField()
     status = models.CharField(max_length=10)
 
