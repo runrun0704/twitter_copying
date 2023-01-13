f5f30cbbed676ebc9b657029428f820f0f032b56
runrun0704 <satoru0704.f@gmail.com>
2023-01-13 13:53:22 +0900

    add api

---
+++ backend/tweet/apis.py
@@ -0,0 +1,8 @@
+from rest_framework import routers
+from .views import TweetViewSet, UserViewSet, TweetTagViewSet
+
+router = routers.DefaultRouter()
+
+router.register(r'tweets', TweetViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'tweettags', TweetTagViewSet)
---
+++ backend/tweet/serializer.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Tweet, User, TweetTag
+
+class TweetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = sTweet
+        fields = (
+            'id', 'title', 'body', 'user_id', 'good_count', 'tags'
+        )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'id', 'name', 'domain', 'password', 'sex', 'age', 'status'
+        )
+
+class TweetTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TweetTag
+        fields = ('tweet_id', 'tag_id')
